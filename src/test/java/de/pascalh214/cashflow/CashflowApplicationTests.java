8a8f6a42dab45d9f6f890c83c9ea2aa7b9ed0292
PascalH214 <pascal02012004@freenet.de>
2026-01-26 19:15:53 +0100

    Initial commit

---
+++ src/test/java/de/pascalh214/cashflow/CashflowApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pascalh214.cashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
